47ae7c047b800aea64e26b46a10987637ca10566
maxim.kirilov <kir.maxim@gmail.com>
2019-02-12 12:02:35 +0200

    scripts robustness (check whether file exists, before load, etc.)

--- install-deps.sh
+++ install-deps.sh
@@ -8,13 +8,10 @@ curl -L https://get.rvm.io | bash -s stable --ruby
 
 # homebrew!
 # you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+[[ "$OSTYPE" == "darwin"* ]] && ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 # https://github.com/rupa/z
 # z, oh how i love you
 mkdir -p ~/dotfiles/z
 curl -L https://raw.github.com/rupa/z/master/z.sh > ~/dotfiles/z/z.sh
 chmod +x ~/dotfiles/z/z.sh
-
-# add this to the bash_profile file if it aint there.
-#   . ~/code/z/z.sh
